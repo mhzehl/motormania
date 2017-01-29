d49a6f5642b5d116ed192adcf2c9787aa21ef9f0
Melvin Zehl <mhzehl@gmail.com>
2017-01-29 21:27:02 +0100

    Add controller actions create and destroy

---
+++ app/controllers/motorcycles_controller.rb
@@ -0,0 +1,31 @@
+class MotorCyclesController < ApplicationController
+
+  def create
+    @brand = Brand.find(params[:brand_id])
+    @motorcycle = Motorcycle.new(motorcycle_params)
+    @motorcycle.brand = @brand
+
+    if @motorcycle.save
+      redirect_to @brand
+    else
+      render "/brand/show"
+    end
+  end
+
+  def destroy
+    brand = Brand.find(params[:brand_id])
+    motorcycle = brand.motorcycle.find(params[:id])
+
+    if @motorcycle.destroy
+      redirect_to :brand, notice: "Motorcycle turned into scrapmetal."
+    else
+      redirect_to :brand, alert: "Motorcycle still has a soul!"
+    end
+  end
+
+  private
+
+  def motorcycle_params
+    params.require(:motorcycle).permit(:type, :category, :production_since, :photo)
+  end
+end
