6592f82d394bc456837139a00ed010966901b48e
Melvin Zehl <mhzehl@gmail.com>
2017-01-29 21:07:40 +0100

    Add nested route for motorcycle resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: "brands#index"
 
-  resources :brands, only: [:show, :create, :update]
+  resources :brands, only: [:show, :create, :update] do
+    resources :motorcycles, only: [:create, :destroy]
+  end
 end
