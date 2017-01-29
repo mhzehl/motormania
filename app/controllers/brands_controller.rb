620fcf7e82799939facac820ee01e2fa50e91762
Melvin Zehl <mhzehl@gmail.com>
2017-01-29 20:22:50 +0100

    Create views and form for Brand

---
+++ app/controllers/brands_controller.rb
@@ -0,0 +1,28 @@
+class BrandsController < ApplicationController
+
+  def index
+    @brands = Brand.all
+    @brand = Brand.new
+  end
+
+  def show
+    @brand = Brand.find(params[:id])
+  end
+
+  def create
+    @brand = Brand.new(brand_params)
+
+    if @brand.save
+      redirect_to root_path
+    else
+      @brands = Brand.all
+      render :index
+    end
+  end
+
+  private
+
+  def brand_params
+    params.require(:brand).permit(:name, :year_of_founding, :country_of_origin)
+  end
+end
